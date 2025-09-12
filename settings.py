2e39a08f2b2288ffc51cf4e17684963a2765a3b3
kinator <behani.julien@gmx.frgit>
2025-09-12 11:28:13 +0200

    merge branches

---
+++ settings.py
@@ -0,0 +1,15 @@
+
+# Fenêtre
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+FPS = 30
+GAME_TITLE = "Galad Islands"
+
+# Carte
+TILE_SIZE = 32
+MAP_WIDTH = 40
+MAP_HEIGHT = 25
+
+def afficher_options():
+    print("Affichage des options...")
+    # Vous pouvez ajouter ici du code pour afficher ou modifier les paramètres du jeu
