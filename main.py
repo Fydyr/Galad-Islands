0f4eb0b093caaf858b52e3f72978565b3447b691
kinator <behani.julien@gmx.frgit>
2025-09-12 14:44:19 +0200

    chore: removed empty files

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import pygame
 from menu import main_menu
 from settings import SCREEN_WIDTH, SCREEN_HEIGHT, GAME_TITLE
+import sys
 
 
 
@@ -15,33 +16,41 @@ ROUGE = (200, 0, 0)
 pygame.init()
 
 
-# Fenêtre principale
-TAILLE_FENETRE = (SCREEN_WIDTH, SCREEN_HEIGHT)
-fenetre = pygame.display.set_mode(TAILLE_FENETRE)
+# Main window
+WINDOW_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+window = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption(GAME_TITLE)
 
 
-
-
 # Placeholder for main menu function
-def menu_principal():
+def ain_menu():
     """Gére le menu principal du jeu
 
     Returns:
         None
     """
-    en_cours = True
-    while en_cours:
+    inGame = False
+
+    running = True
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
-                en_cours = False
+                running = False
 
-    fenetre.fill(BLANC)  # Efface l'écran
+        if not inGame:
+            if main_menu() == 'quit':
+                running = False
+        
+        else:
+            pass
+
+
+    window.fill(BLANC)  # Efface l'écran
 
     pygame.display.flip()
 
 
 if __name__ == "__main__":
-    #menu_principal()
     main_menu()
     pygame.quit()
+    sys.exit()
