f31a231d057cf338eb48506bfdb176f532aa021d
Ethann Cailliau <231699035+ethann59@users.noreply.github.com>
2025-09-15 11:21:01 +0200

    Mise à jour des taux de mines et d'îles génériques, ajustement des dimensions des images et amélioration de l'affichage des éléments sur la carte.

--- settings.py
+++ settings.py
@@ -13,7 +13,8 @@ GAME_TITLE = "Galad Islands"
 MAP_WIDTH = 30  # nombre de cases en largeur (modifiable)
 MAP_HEIGHT = 30 # nombre de cases en hauteur (modifiable)
 TILE_SIZE = 32  # taille d'une case en pixels
-MINE_RATE = math.ceil(MAP_WIDTH * MAP_HEIGHT * 0.02)
+MINE_RATE = math.ceil(MAP_WIDTH * MAP_HEIGHT * 0.01) # taux de mines (2% de la carte)
+GENERIC_ISLAND_RATE = math.ceil(MAP_WIDTH * MAP_HEIGHT * 0.025) # taux d'îles génériques (10% de la carte)
 
 def afficher_options():
     print("Affichage des options...")
