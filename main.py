8fc663722846d0555e9b0114e843cf3a87b8245d
kaldex0 <150331061+kaldex0@users.noreply.github.com>
2025-09-19 14:14:49 +0200

    ajout de 2-3 modif pour perfectionner l'ouverture du menu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Importations
 import pygame
-from menu import main_menu
+from menuV7 import main_menu
 from game import game
 from settings import SCREEN_WIDTH, SCREEN_HEIGHT, GAME_TITLE
 import sys
@@ -16,46 +16,15 @@ ROUGE = (200, 0, 0)
 # Initialize Pygame
 pygame.init()
 
-
-# Main window
-WINDOW_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
-window = pygame.display.set_mode(WINDOW_SIZE)
-pygame.display.set_caption(GAME_TITLE)
-
-
-# Placeholder for main menu function
+# Main function sans création de fenêtre (le menu s'en charge)
 def main():
     """Gére la logique entre le menu et le jeu
 
     Returns:
         None
     """
-    inGame = False
-
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-        if not inGame:
-            menu_choice = main_menu()
-            if menu_choice == 'quit':
-                running = False
-            if menu_choice == 'play':
-                inGame = True
-        
-        else:
-            game()
-            inGame = False
-
-
-    window.fill(BLANC)  # Efface l'écran
-
-    pygame.display.flip()
-
-    pygame.quit()
-    sys.exit()
+    # Lancement direct du menu principal
+    main_menu()
 
 
 if __name__ == "__main__":
