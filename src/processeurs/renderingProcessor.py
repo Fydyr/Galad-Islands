f137b1e2b4d9c4ee6eb02fcaa30bfda5d5a76dd6
kinator <behani.julien@gmx.frgit>
2025-09-22 09:06:03 +0200

    fix: game.py import

--- src/processeurs/renderingProcessor.py
+++ src/processeurs/renderingProcessor.py
@@ -2,6 +2,7 @@ import esper
 import pygame
 from src.components.properties.positionComponent import PositionComponent
 from src.components.properties.spriteComponent import SpriteComponent
+from settings import TILE_SIZE
 
 class RenderProcessor(esper.Processor):
     def __init__(self, screen: pygame.surface):
@@ -10,6 +11,6 @@ class RenderProcessor(esper.Processor):
 
     def process(self):
         for ent, (pos, sprite) in esper.get_components(PositionComponent, SpriteComponent):
-            # self.screen.blit(pygame.transform.scale(pygame.image.load(sprite.image_path), (pos.x, pos.y)), (sprite.height, sprite.width))
-            rotated_image = pygame.transform.rotate(pygame.image.load(sprite.image_path), pos.direction)
-            self.screen.blit(rotated_image, (pos.x, pos.y))
+            rotated_image = pygame.transform.rotate(pygame.image.load(sprite.load_sprite()), pos.direction)
+            # self.screen.blit(rotated_image, (pos.x, pos.y))
+            self.screen.blit(pygame.transform.scale(rotated_image, (sprite.height * TILE_SIZE, sprite.width * TILE_SIZE)))
