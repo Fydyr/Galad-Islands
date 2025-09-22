cc158f9f11d56aa7b65adcd5c4094a3bf3898bd1
kinator <behani.julien@gmx.frgit>
2025-09-22 16:00:01 +0200

    fix: rendering issue

--- src/components/properties/positionComponent.py
+++ src/components/properties/positionComponent.py
@@ -2,9 +2,7 @@ from dataclasses import dataclass as component
 
 @component
 class PositionComponent:
-    def __init__(self, x=0.0, y=0.0, height=0.0, width=0.0, direction=0.0):
+    def __init__(self, x=0.0, y=0.0, direction=0.0):
         self.x: float = x
         self.y: float = y
-        self.height: float = height
-        self.width: float = width
         self.direction: float = direction
