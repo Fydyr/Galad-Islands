3a21d9c641ca5706964c7c060ab9d25818d5875d
kinator <behani.julien@gmx.frgit>
2025-09-22 11:19:54 +0200

    fix: full movement and sprite display

--- src/processeurs/movementProcessor.py
+++ src/processeurs/movementProcessor.py
@@ -8,5 +8,5 @@ class MovementProcessor(esper.Processor):
     def process(self):
         for ent, (vel, pos) in esper.get_components(Velocity, Position):
             if vel.currentSpeed != 0:
-                pos.x += vel.currentSpeed * cos(radians(pos.direction))
-                pos.y += vel.currentSpeed * sin(radians(pos.direction))
+                pos.x -= vel.currentSpeed * cos(radians(pos.direction))
+                pos.y -= vel.currentSpeed * sin(radians(pos.direction))
