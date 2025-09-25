fb907a2a874efad4b122babe901de4582c48ff04
Ethann Cailliau <231699035+ethann59@users.noreply.github.com>
2025-09-25 14:53:52 +0200

    fix(settings): correction des taux de mines et d'îles génériques

--- settings.py
+++ settings.py
@@ -129,8 +129,8 @@ def calculate_adaptive_tile_size():
     return adaptive_size
 
 TILE_SIZE = calculate_adaptive_tile_size()  # taille d'une case en pixels (adaptative)
-MINE_RATE = math.ceil(MAP_WIDTH * MAP_HEIGHT * 0.01) # taux de mines (2% de la carte)
-GENERIC_ISLAND_RATE = math.ceil(MAP_WIDTH * MAP_HEIGHT * 0.025) # taux d'îles génériques (10% de la carte)
+MINE_RATE = math.ceil(MAP_WIDTH * MAP_HEIGHT * 0.02) # taux de mines (2% de la carte)
+GENERIC_ISLAND_RATE = math.ceil(MAP_WIDTH * MAP_HEIGHT * 0.05) # taux d'îles génériques (5% de la carte)
 
 # Paramètres de caméra
 CAMERA_SPEED = 200  # pixels par seconde
