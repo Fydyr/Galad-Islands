6ff68ce5f44d15bd7f15eccb83707047073fe440
kinator <behani.julien@gmx.frgit>
2025-09-25 15:12:53 +0200

    fix: functional cooldown

--- src/components/properties/radiusComponent.py
+++ src/components/properties/radiusComponent.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass as component
 
 @component
 class RadiusComponent:
-    def __init__(self, radius=0.0, angle=0.0, omnidirectional=False, can_shoot_from_side=False, bullets_front=0, bullets_sides=0, cooldown=0.0):
+    def __init__(self, radius=0.0, angle=0.0, omnidirectional=False, can_shoot_from_side=False, bullets_front=0, bullets_sides=0, cooldown=0.0, bullet_cooldown=0.0):
         self.radius: float = radius
         self.angle: float = angle
         self.omnidirectional: bool = omnidirectional
@@ -10,4 +10,5 @@ class RadiusComponent:
         self.bullets_front: int = bullets_front
         self.bullets_side: int = bullets_sides
         self.cooldown: float = cooldown
+        self.bullet_cooldown: float = bullet_cooldown
     
