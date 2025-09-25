31b34682ffd67b2d23c02ae6353365bb6a13018a
kinator <behani.julien@gmx.frgit>
2025-09-25 13:37:54 +0200

    fix(renderingProcessor): updated sprite and rendering processeur

--- src/components/properties/spriteComponent.py
+++ src/components/properties/spriteComponent.py
@@ -1,14 +1,17 @@
 from dataclasses import dataclass as component
+import pygame
 
 @component
 class SpriteComponent:
     
-    def __init__ ( self, image_path: str = "", width=0.0, height=0.0):
+    def __init__ ( self, image_path: str = "", width=0.0, height=0.0, surface=None):
         self.image_path: str = image_path  # Chemin vers l'image du sprite dans le dossier assets
         self.width: float = width
         self.height: float = height
-        
+        self.surface: pygame.Surface|None = surface
+        self.load_sprite()
 
     def load_sprite(self):
         """Retourne le chemin du sprite (à charger avec pygame ou autre moteur graphique)"""
-        return self.image_path
+        image = pygame.image.load(self.image_path).convert_alpha()
+        self.surface = pygame.transform.scale(image, (self.width, self.height))
