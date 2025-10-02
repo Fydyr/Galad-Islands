ba34bc718f04e8e3c8619247365110ececea8487
Talion <167514459+AlluinEdouard@users.noreply.github.com>
2025-10-03 00:07:05 +0200

    fix: speArchitectComponent.py, unitFactory.py, CpacitiesSpecialesProcessor.py, playerControlProcessor.py

--- src/components/properties/ability/speArchitectComponent.py
+++ src/components/properties/ability/speArchitectComponent.py
@@ -4,21 +4,15 @@ from typing import List, Dict, Optional, Tuple
 @component
 class SpeArchitect:
     
-    def __init__(self, is_active: bool = False, available: bool = False, radius: float = 0.0, reload_factor: float = 0.0, affected_units: Optional[List[int]] = None, duration: float = 0.0, timer: float = 0.0):
-        self.is_active: bool = False
-        self.available: bool = False
-        self.radius: float = 0.0 #Rayon d'effet de la capacité
-        self.reload_factor: float = 0.0 # Divise la durée  de rechargement par 2
-        self.affected_units: List[int] = None # IDs des unités affectées
-        self.duration: float = 0.0 # Durée de l'effet
-        self.timer: float = 0.0 # Temps restant 
-    
+    def __init__(self, is_active: bool = False, available: bool = True, radius: float = 0.0, reload_factor: float = 0.0, affected_units: Optional[List[int]] = None, duration: float = 0.0, timer: float = 0.0):
+        self.is_active: bool = is_active
+        self.available: bool = available
+        self.radius: float = radius  # Rayon d'effet de la capacité
+        self.reload_factor: float = reload_factor  # Divise la durée de rechargement par 2
+        self.affected_units: List[int] = affected_units if affected_units is not None else []  # IDs des unités affectées
+        self.duration: float = duration  # Durée de l'effet
+        self.timer: float = timer  # Temps restant 
 
-    def __post_init__(self):
-        if self.affected_units is None:
-            self.affected_units = []
-
-    
     def activate(self, affected_units: List[int], duration: float = 0.0):
         """ 
         Active le rechargement automatique sur les zeppelins dans le rayon.
@@ -30,7 +24,6 @@ class SpeArchitect:
         self.affected_units = affected_units.copy()
         self.duration = duration
         self.timer = duration
-
 
     def update(self, dt):
         """
@@ -44,5 +37,3 @@ class SpeArchitect:
                 self.available = True
                 self.timer = 0.0
                 self.affected_units.clear()
-                
-    
