9865ce706ff7f4ae613b9c6d12f5cbcc1f741f63
kinator <behani.julien@gmx.frgit>
2025-10-03 06:48:16 +0200

    fix: unitType correct properties

--- src/factory/unitType.py
+++ src/factory/unitType.py
@@ -1,10 +1,35 @@
 from enum import Enum
 
-class UnitType(str, Enum):
-    SCOUT = "scout"
-    MARAUDEUR = "maraudeur"
-    LEVIATHAN = "Leviathan"
-    DRUID = "druid"
-    ARCHITECT = "architect"
-    ATTACK_TOWER = "attack_tower"
-    HEAL_TOWER = "heal_tower"
+class UnitClass(Enum):
+    """Enumeration for different unit classes in the game."""
+    SCOUT = 0
+    MARAUDEUR = 1  
+    LEVIATHAN = 2
+    DRUID = 3
+    ARCHITECT = 4
+    
+    @property
+    def display_name(self, enemy: bool) -> str:
+        """Get the display name for the unit class."""
+        if enemy:
+            names = {
+                UnitClass.SCOUT: "Gargoille",
+                UnitClass.MARAUDEUR: "Griffon", 
+                UnitClass.LEVIATHAN: "Dragon",
+                UnitClass.DRUID: "Litch",
+                UnitClass.ARCHITECT: "Goblin ingénieur"
+            }
+        else:
+            names = {
+                UnitClass.SCOUT: "Zasper",
+                UnitClass.MARAUDEUR: "Barhamus", 
+                UnitClass.LEVIATHAN: "Draupnir",
+                UnitClass.DRUID: "Druid",
+                UnitClass.ARCHITECT: "Architect"
+            }
+        return names.get(self, self.name.title())
+
+    @property
+    def get_classe(self) -> str:
+        """Get the display name for the unit class."""
+        return self.value
