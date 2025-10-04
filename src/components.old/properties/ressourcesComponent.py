18193caa68349273c2af9437e60ee6c83a663f99
Ethann Cailliau <231699035+ethann59@users.noreply.github.com>
2025-10-04 16:01:14 +0200

    refactor: Suppresion des anciens composants qui étaient conservés au cas où

--- src/components.old/properties/ressourcesComponent.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from ..base_component import GameplayComponent
-
-@dataclass
-class ResourcesComponent(GameplayComponent):
-    """Component representing entity's resources (gold, etc.)."""
-    def __init__(self, gold: int = 0):
-        self.gold = gold
-    
--- src/components.old/properties/teamComponent.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from .team_enum import Team
-
-@dataclass
-class TeamComponent:
-    """Component representing the team/faction of an entity."""
-    def __init__(self, team: Team = Team.NEUTRAL):
-        self.team = team
-   
