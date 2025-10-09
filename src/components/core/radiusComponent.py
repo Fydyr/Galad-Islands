9829f649c2402be9c4f0c364eb250b700cc3443a
kinator <behani.julien@gmx.fr>
2025-10-09 14:24:09 +0200

    fix: milti firing on sides and front

--- src/components/core/radiusComponent.py
+++ src/components/core/radiusComponent.py
@@ -2,11 +2,12 @@ from dataclasses import dataclass as component
 
 @component
 class RadiusComponent:
-    def __init__(self, radius=0.0, angle=0.0, omnidirectional=False, can_shoot_from_side=False, bullets_front=0, bullets_sides=0, cooldown=0.0, bullet_cooldown=0.0):
+    def __init__(self, radius=0.0, angle=0.0, omnidirectional=False, can_shoot_from_side=False, lateral_shooting=False, bullets_front=0, bullets_sides=0, cooldown=0.0, bullet_cooldown=0.0):
         self.radius: float = radius
         self.angle: float = angle
         self.omnidirectional: bool = omnidirectional
         self.can_shoot_from_side: bool = can_shoot_from_side
+        self.lateral_shooting: bool = lateral_shooting
         self.bullets_front: int = bullets_front
         self.bullets_side: int = bullets_sides
         self.cooldown: float = cooldown
