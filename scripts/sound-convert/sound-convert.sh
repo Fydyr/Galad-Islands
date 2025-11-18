296641246a59d65717d86fa93174eb78f142d573
Ethann Cailliau <231699035+ethann59@users.noreply.github.com>
2025-11-18 15:05:54 +0100

    feat: ajout de nouveaux fichiers sonores et mise à jour du script de conversion pour inclure les fichiers WAV

--- scripts/sound-convert/sound-convert.sh
+++ scripts/sound-convert/sound-convert.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Script de conversion MP3 -> OGG avec ffmpeg
-# Il convertit tous les fichiers .mp3 du répertoire courant en .ogg
+# Script de conversion MP3/WAV -> OGG avec ffmpeg
+# Il convertit tous les fichiers .mp3 et .wav du répertoire courant en .ogg
 # Usage: ./sound-convert.sh
 
-for f in *.mp3; do
+for f in *.mp3 and *.wav; do
     # Supprime l'extension .mp3 pour créer le nom de sortie
     base="${f%.mp3}"
     echo "Conversion de $f en ${base}.ogg ..."
